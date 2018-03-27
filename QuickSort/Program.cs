6c7e4030e8e798f0af798601d8a91d13a36248f3
Admin <Admin@WIN-118OHA59S8I>
2018-03-27 21:44:41 +0500

    Up

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -1,15 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace QuickSort
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void QuickSort(int[] array, int start, int end)
         {
+            if (end == start) return;
+            var pivot = array[end];
+            var storeIndex = start;
+            for (int i = start; i <= end - 1; i++)
+                if (array[i] <= pivot)
+                {
+                    var t = array[i];
+                    array[i] = array[storeIndex];
+                    array[storeIndex] = t;
+                    storeIndex++;
+                }
+
+            var n = array[storeIndex];
+            array[storeIndex] = array[end];
+            array[end] = n;
+            if (storeIndex > start) QuickSort(array, start, storeIndex - 1);
+            if (storeIndex < end) QuickSort(array, storeIndex + 1, end);
+        }
+
+        public static void QuickSort(int[] array)
+        {
+            if (array.Length == 0) return;
+            QuickSort(array, 0, array.Length - 1);
+        }
+
+        static Random random = new Random();
+
+        public static int[] GenerateArray(int length)
+        {
+            var array = new int[length];
+            for (int i = 0; i < array.Length; i++)
+                array[i] = random.Next();
+            return array;
+        }
+
+        public static void Main()
+        {
+
         }
     }
 }
+
---
+++ QuickSortTests/UnitTest1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace QuickSortTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestSort()
+        {
+            var array = QuickSort.Program.GenerateArray(3);
+            QuickSort.Program.QuickSort(array);
+
+            Assert.AreEqual(array[0] < array[1], true);
+            Assert.AreEqual(array[1] < array[2], true);
+        }
+
+        [TestMethod]
+        public void TestSortHundred()
+        {
+            var array = new int[100];
+            for (int i = 0; i < 100; i++) array[i] = 1;
+            QuickSort.Program.QuickSort(array);
+
+            Assert.AreEqual(array[0] <= array[1], true);
+        }
+
+        [TestMethod]
+        public void TestSortThousand()
+        {
+            var array = QuickSort.Program.GenerateArray(1000);
+            QuickSort.Program.QuickSort(array);
+            Random x = new Random();
+
+            for (int i = 0; i < 10; i++) Assert.AreEqual(array[x.Next(0, 500)] <= array[x.Next(500, 999)], true);
+        }
+
+        [TestMethod]
+        public void TestSortEmpty()
+        {
+            var array = QuickSort.Program.GenerateArray(0);
+            QuickSort.Program.QuickSort(array);
+        }
+
+        [TestMethod]
+        public void TestSortBig()
+        {
+            var array = QuickSort.Program.GenerateArray(100000000);
+            QuickSort.Program.QuickSort(array);
+            Random x = new Random();
+
+            Assert.AreEqual(array[0] < array[x.Next(1, array.Length - 1)], true);
+        }
+    }
+}
